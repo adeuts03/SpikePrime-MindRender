6d0683ea84f47e39ac3f54e6a432915d70af2cf1
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2022-06-15 14:53:49 -0400

    Update SpikeSend.py

--- Spike-ESP connection/SpikeSend.py
+++ Spike-ESP connection/SpikeSend.py
@@ -1,75 +1,79 @@
 '''
 To do
-- name this code
 - clean up code in general
 - comment out code and describe what it's doing
 - add debug things (the first chunk of code is essentially a test to make sure serial is working, 
   that can probably be thrown into a debug section that can be turned on/off)
+
+Changelog
+6/15/22
+- 
+6/14/22
+- gave code a proper name
 '''
 
 
 
 # Make sure you have Backpack_Code.py "installed" (aka saved) on the Spike and send_udp.py installed on the ESP
 
-import os
+# initialize the hub and get your imports
 from Backpack_Code import Backpack
-import sys
-
-#initialize hub
-
 import hub, math, utime
 from hub import motion
 from spike import PrimeHub
 
 
 
-file = '''
-print('testing')
-'''
-file = file.replace("\'",'"')
+# file = '''
+# print('testing')
+# '''
+# file = file.replace("\'",'"')
 
+# make sure you change the port to wherever you have the ESP plugged in on the Prime
 dongle = Backpack(hub.port.F, verbose = True) 
 
 dongle.setup()
-filename = 'test.py'
-dongle.load(filename,file)
-reply = dongle.get(filename)
-print(reply == file)
+# filename = 'test.py'
+# dongle.load(filename,file)
+# reply = dongle.get(filename)
+# print(reply == file)
 
 
-print("Testing done.")
+# print("Testing done.")
 print("_________________________________________________________")
 
 
-
+# on first run of this code, we need to get the ESP to import our send library
 dongle.ask('from esp_send import send_message')
+# we then need to set up our connection over wifi - change the IP address address and port below
+# do NOT change anything else - leave the quotes, slashes, and commas the way they are
+#                                   IP          port 
 dongle.ask('x = send_message(\"10.245.95.56\", 21024)')
 
-# dongle.ask('x.send(\'123\')')
-# utime.sleep(5)
 
-iterations = 0
-move = 1300
-while (iterations < 2):
-    print("New iteration, get ready to move.")
-    print("3")
-    utime.sleep(.1)
-    print("2")
-    utime.sleep(.1)
-    print("1")
-    utime.sleep(.1)
-    print("Move!")
-    
-    utime.sleep(.1)
-    print('Processing...')
-    iterations += 1
-    move = move + iterations
-    message = "x.send(\'" + str(move) + "\')"
-    print("message is", message)
-    dongle.ask(message)
-    utime.sleep(.1)
 
-print("End of program. ***NOT*****")
+# iteration = 0
+# move = 1300
+# while (iteration < 2):
+#     print("New iteration, get ready to move.")
+#     print("3")
+#     utime.sleep(.1)
+#     print("2")
+#     utime.sleep(.1)
+#     print("1")
+#     utime.sleep(.1)
+#     print("Move!")
+# 
+#     utime.sleep(.1)
+#     print('Processing...')
+#     iteration += 1
+#     move = move + iteration
+#     message = "x.send(\'" + str(move) + "\')"
+#     print("message is", message)
+#     dongle.ask(message)
+#     utime.sleep(.1)
+# 
+# print("End of program. ***NOT*****")
 
 hub.display.clear()
 utime.sleep(.5)
