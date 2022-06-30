4492c26e806fb857fe0e929337a8c5321c443d7f
alan2021233 <46859354+adeuts03@users.noreply.github.com>
2022-06-30 13:58:46 -0400

    Moved MR_car to different folder. Added new file for spike3. Removed comments from MR_car.py

--- Spike-ESP connection/MR_car.py
+++ Spike-ESP connection/MR_car.py
@@ -7,44 +7,34 @@ import utime
 import struct
 from micropython import const
 
-_ADV_TYPE_FLAGS = const(0x01)            # GAP通信における制御情報
-_ADV_TYPE_NAME = const(0x09)            # 
+_ADV_TYPE_FLAGS = const(0x01)
+_ADV_TYPE_NAME = const(0x09)
 デバイスの名称(完全版)(28Bを超えるなら0x08の短縮名称を使う)
-_ADV_TYPE_UUID16_COMPLETE = const(0x3)# 
+_ADV_TYPE_UUID16_COMPLETE = const(0x3)
 利用可能な16ビットUUID(全てのUUIDをAD Dataに含める場合)
 _ADV_TYPE_UUID32_COMPLETE = const(0x5)
 _ADV_TYPE_UUID128_COMPLETE = const(0x7)
-_ADV_TYPE_UUID16_MORE = const(0x2)    # 
+_ADV_TYPE_UUID16_MORE = const(0x2)
 利用可能な16ビットUUID(一部のUUIDのみAD Dataに含める場合)
 _ADV_TYPE_UUID32_MORE = const(0x4)
 _ADV_TYPE_UUID128_MORE = const(0x6)
-_ADV_TYPE_APPEARANCE = const(0x19)    # デバイスの機能種別名
-_ADV_TYPE_SPECIFIC_DATA = const(0xFF)    # 
+_ADV_TYPE_APPEARANCE = const(0x19)
+_ADV_TYPE_SPECIFIC_DATA = const(0xFF)
 任意の送信データ(ベンダー固有のデータ)を送れる
-_ADV_DATA_COMPANY_ID = const(0xFFFF)    # ベンダーのID
+_ADV_DATA_COMPANY_ID = const(0xFFFF)
 
-# gap_advertise(adv_data = ...)に渡されるメッセージパケットを生成します。
-# limited_disc : False=LE 一般検出可能モード　True=LE 限定検出可能モード
-# br_edr : False=BR/EDRはサポートされていません　True=0x18?
-# name : 
-デバイス名で任意につけてOK、iphone等のアプリ画面上にこの装置名が表示されます
-# services : サービスのUUID
-# appearance : 
-https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.gap.appearance.xml
 def advertising_payload(limited_disc=False, br_edr=False, name=None, 
 services=None, appearance=0, free=None):
     payload = bytearray()
 
-    # アドバータイジングパケットの１データをパックします。
     def _append(adv_type, value):
         nonlocal payload
         payload += struct.pack("BB", len(value) + 1, adv_type) + value
 
     _append(
-        _ADV_TYPE_FLAGS,
-        struct.pack("B", (0x01 if limited_disc else 0x02) + (0x18 if 
-br_edr else 0x04)),
-    )
+        _ADV_TYPE_FLAGS, 
+        struct.pack("B", (0x01 if limited_disc else 0x02) + (0x18 if br_edr else 0x04)),
+        )
 
     if name:
         _append(_ADV_TYPE_NAME, name)
@@ -59,8 +49,6 @@ br_edr else 0x04)),
             elif len(b) == 16:
                 _append(_ADV_TYPE_UUID128_COMPLETE, b)
 
-    # 
-org.bluetooth.characteristic.gap.appearance.xmlを参照してください。（デバイスの機能種別名）
     if appearance:
         _append(_ADV_TYPE_APPEARANCE, struct.pack("<h", appearance))
 
@@ -97,48 +85,38 @@ self._ble.gatts_register_services((_UART_SERVICE,))
 services=[_UART_UUID])
         self._advertise()
 
-    # 接続状態の取得
     def is_connected(self):
         return len(self._connections) > 0
 
-    # データ送信
     def send(self, data):
         if self.is_connected():
             for handle in self._connections:
                 self._ble.gatts_notify(handle, self._handle_rx, data)
                 self._ble.gatts_notify(handle, self._handle_tx, data)
 
-    # イベントコールバック
     def _irq(self, event, data):
-        # セントラルに接続
         if event == _IRQ_CENTRAL_CONNECT:
             conn_handle, _, _ = data
             self._connections.add(conn_handle)
             print("Connection", conn_handle)
 
-        # セントラルから切断
         elif event == _IRQ_CENTRAL_DISCONNECT:
             print("Disconnected")
             conn_handle, _, _ = data
             if conn_handle in self._connections:
                 self._connections.remove(conn_handle)
                 
-            #self._advertise()
             print("Disconnected", conn_handle)
 
-        # セントラルからの書き込み
         elif event == _IRQ_GATTS_WRITE:
             print('Read')
             conn_handle, value_handle = data
-            # データを読み込む
             value = self._ble.gatts_read(value_handle)
             if value_handle == self._handle_rx:
                 print("Rx", value)
 
-    # Advertise開始
     def _advertise(self):
         self._ble.gap_advertise(500000, adv_data=self._payload)
-        #self._ble.gap_advertise(500, "MindRender")
         print("Advertise")
 
 hub = PrimeHub()
---
+++ Spike-ESP connection/MR_car_spike3.py
@@ -0,0 +1,7 @@
+import util
+import imu
+from math import *
+import bluetooth
+import utime
+import struct
+from micropython import const
